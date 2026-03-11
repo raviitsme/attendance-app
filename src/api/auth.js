c758cbaa297cdb1d2e9a31765c09839370068f93
Ravi Mohan <its.ravimohan01@gmail.com>
2026-03-11 19:21:37 +0530

    fixed server side

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL : "http://localhost:3000/auth",
+    baseURL : "https://attendance-app-backend-y8y2.onrender.com/auth",
     withCredentials : true
 });
 
